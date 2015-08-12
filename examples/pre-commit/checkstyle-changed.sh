7dd8594ccb21fcbd509d8bef4966504bf11a7a21
Josh Friend <jfriend@michiganlabs.com>
2015-08-12 18:37:01 -0400

    Only run checkstyle on java files

--- examples/pre-commit/checkstyle-changed.sh
+++ examples/pre-commit/checkstyle-changed.sh
@@ -2,7 +2,7 @@
 
 # Run checkstyle on changed files
 
-CHANGED_FILES=$(git diff --staged --name-status | grep '[MA].*\.java' | awk '{ print $2 }')
+CHANGED_FILES=$(git diff --staged --name-status | grep '[MA]' | awk '{ print $2 }')
 if [ "$CHANGED_FILES" ]; then
     checkstyle -c app/config/checkstyle/checkstyle.xml $CHANGED_FILES
     exit $?
